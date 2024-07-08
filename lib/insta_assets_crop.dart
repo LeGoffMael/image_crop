f33ea33ab49ab03de8f02c5422432f8754d1cbb8
Maël Le Goff <legoffmael@gmail.com>
2024-07-08 20:21:43 +0900

    ♻️ add CropTransform layoutSize param

--- lib/insta_assets_crop.dart
+++ lib/insta_assets_crop.dart
@@ -7,7 +7,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-import 'package:insta_assets_crop/src/crop_transform.dart';
 
 part 'src/insta_assets_crop.dart';
 part 'src/crop.dart';
+part 'src/crop_transform.dart';
